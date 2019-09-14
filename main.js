4844dc2de39c5c7f87a7ac85bbed50dae8cbca1b
kevin johnson <kevinj902302@gmail.com>
2019-09-14 13:34:24 -0600

    Built functions to add players names on different pages

--- main.js
+++ main.js
@@ -1,25 +1,49 @@
-var player1Name = document.querySelector('.player1-name');
-var player2Name = document.querySelector('.player2-name');
 var playNowButton = document.querySelector('.play-button');
-
-
+var playButton2 = document.querySelector(".play-button2");
+var players = [];
+var cards = document.querySelectorAll(".card");
 
 
 
 function show(shown, hidden) {
   document.getElementById(shown).style.display='block';
   document.getElementById(hidden).style.display='none'
-  return false;
+
 }
 
-// function changeNames() {
-//   var newName2 = player2Name.innertext;
-//   var newName1 = player1Name.innertext;
-//   document.querySelector("player1-name").innertext === newName1;
-//   document.querySelector("player2-name").innertext === newName2;
-// }
-//
-// playNowButton.addEventListener('click', function(event) {
-//   show();
-//   changeNames();
-// });
+function changeNames1() {
+  var newName1 = document.querySelector(".player1").value;
+  var newName2 = document.querySelector(".player2").value;
+
+  players.push(newName1.toUpperCase(), newName2.toUpperCase())
+}
+
+function changeNames2() {
+  var newName1 = document.querySelector(".player1").value;
+  var newName2 = document.querySelector(".player2").value;
+
+  players.push(newName1.toUpperCase(), newName2.toUpperCase())
+}
+
+playNowButton.addEventListener('click', function(event) {
+  changeNames1();
+  show("Page2","Page1");
+  document.querySelector(".player1-name").innerText = players[0];
+  document.querySelector(".player2-name").innerText = players[1];
+
+});
+
+playButton2.addEventListener('click', function(event) {
+  changeNames2();
+  show("Page3","Page2");
+  document.querySelector(".player-1-name").innerText = players[0];
+  document.querySelector(".player-2-name").innerText = players[1];
+})
+
+cards.forEach(function(card){
+  return card.addEventListener('click', flipcard)
+})
+
+function flipcard(e) {
+  console.log('flipped',e.target.id);
+  }
