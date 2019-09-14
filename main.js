5616a482af7e9b2fe808b25152dbef0d408d761d
kevin johnson <kevinj902302@gmail.com>
2019-09-14 10:51:03 -0600

    Finished most of the styling, tried to add some JS

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
+var player1Name = document.querySelector('.player1-name');
+var player2Name = document.querySelector('.player2-name');
+var playNowButton = document.querySelector('.play-button');
+
+
+
+
+
 function show(shown, hidden) {
   document.getElementById(shown).style.display='block';
-  document.getElementById(hidden).style.display='none';
+  document.getElementById(hidden).style.display='none'
   return false;
 }
+
+// function changeNames() {
+//   var newName2 = player2Name.innertext;
+//   var newName1 = player1Name.innertext;
+//   document.querySelector("player1-name").innertext === newName1;
+//   document.querySelector("player2-name").innertext === newName2;
+// }
+//
+// playNowButton.addEventListener('click', function(event) {
+//   show();
+//   changeNames();
+// });
