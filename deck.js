f56477d77b0c676393387722237e5df2e71efbfb
kevin johnson <kevinj902302@gmail.com>
2019-09-15 21:06:54 -0600

    Added styling to game section and added js functionality

---
+++ deck.js
@@ -0,0 +1,23 @@
+class Deck {
+  constructor(task) {
+
+    this.item = data;
+    this.complete = false;
+    this.id = Date.now();
+  }
+}
+
+function shuffle() {
+  let shuffledImages = shuffle(imgElementsArray);
+  for(i=0; i<shuffledImages.length; i++) {
+
+  cardElements[i].appendChild(shuffledImages[i]);
+}
+
+function checkSelectedCards() {
+
+}
+
+function moveToMatched() {
+
+}
--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 var playNowButton = document.querySelector('.play-button');
 var playButton2 = document.querySelector(".play-button2");
-var players = [];
+// var players = [];
+var players = [
+  {name:null, turn:true, points:0, counter:0},
+  {name:null,turn:false,points:0,counter:0}
+];
+var deck = []
 var cards = document.querySelectorAll(".card");
+var player1Input = document.querySelector('.player1');
+var player2Input = document.querySelector('.player2');
 
+function
+
+function startGame() {
+
+
+}
 
 
 function show(shown, hidden) {
-  document.getElementById(shown).style.display='block';
+  document.getElementById(shown).style.display='inline-block';
   document.getElementById(hidden).style.display='none'
 
 }
@@ -14,36 +27,42 @@ function show(shown, hidden) {
 function changeNames1() {
   var newName1 = document.querySelector(".player1").value;
   var newName2 = document.querySelector(".player2").value;
-
-  players.push(newName1.toUpperCase(), newName2.toUpperCase())
-}
-
-function changeNames2() {
-  var newName1 = document.querySelector(".player1").value;
-  var newName2 = document.querySelector(".player2").value;
-
+  players[0].name = newName1;
+  players[1].name = newName2;
   players.push(newName1.toUpperCase(), newName2.toUpperCase())
 }
 
 playNowButton.addEventListener('click', function(event) {
-  changeNames1();
-  show("Page2","Page1");
-  document.querySelector(".player1-name").innerText = players[0];
-  document.querySelector(".player2-name").innerText = players[1];
-
+  console.log(player1Input.value);
+  if (player1Input.value === '' || player2Input.value === '') {
+     playNowButton.disabled = true;
+     noNameError();
+  } if (player1Input.value !== '' && player2Input.value !== ''){
+    console.log('1');
+    playNowButton.disabled = false;
+    changeNames1();
+    show("Page2","Page1");
+    document.querySelector(".player1-name").innerText = players[0].name;
+    document.querySelector(".player2-name").innerText = players[1].name;
+  }
 });
 
 playButton2.addEventListener('click', function(event) {
-  changeNames2();
   show("Page3","Page2");
-  document.querySelector(".player-1-name").innerText = players[0];
-  document.querySelector(".player-2-name").innerText = players[1];
+  document.querySelector(".player-1-name").innerText = players[0].name;
+  document.querySelector(".player-2-name").innerText = players[1].name;
 })
 
 cards.forEach(function(card){
-  return card.addEventListener('click', flipcard)
+  return card.addEventListener('click', flipCard);
 })
 
-function flipcard(e) {
-  console.log('flipped',e.target.id);
+function flipCard() {
+  this.classList.toggle('flip');
   }
+
+  function noNameError() {
+  var nameError = document.getElementById("empty-name-error");
+  playNowButton.style.borderColor = '#FFC30C';
+  nameError.style.display = 'block';
+}
