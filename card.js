30a1a14bfab3bbcb19e5301c5a0a2cf0b8f74da6
kevin johnson <kevinj902302@gmail.com>
2019-11-02 21:25:53 -0600

    Created card class

---
+++ card.js
@@ -0,0 +1,22 @@
+class Card {
+  constructor() {
+  this.id = 0;
+  this.image = 'logo';
+  this.matchInfo = this.image;
+  this.matched = false;
+}
+
+getId(counter) {
+  this.id = counter;
+}
+
+match() {
+  this.matched = true;
+}
+
+
+
+
+
+
+}
