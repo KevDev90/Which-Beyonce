2d1f804e44c4cc48157375c54ab4c06d4579e346
kevin johnson <kevinj902302@gmail.com>
2019-11-02 21:25:26 -0600

    created deck class

--- deck.js
+++ deck.js
@@ -1,23 +1,27 @@
 class Deck {
   constructor(task) {
-
+    this.cards = [];
+    this.matchedCards= [];
+    this.selectedCards= [];
     this.item = data;
     this.complete = false;
     this.id = Date.now();
+    this.matches = 0;
   }
+
+checkSelectedCards() {
+  var result = this.selectedCards[0].image === this.selectedCards[1].image;
+    return result;
 }
 
-// function shuffle() {
-//   let shuffledImages = shuffle(imgElementsArray);
-//   for(i=0; i<shuffledImages.length; i++) {
-//
-//   cardElements[i].appendChild(shuffledImages[i]);
-// }
-//
-// // function checkSelectedCards() {
-// //
-// // }
-// //
-// // function moveToMatched() {
-// //
-// // }
+ moveToMatched(card1, card2) {
+    var index1 = this.selectedCards[0].id - 1;
+    var index2 = this.selectedCards[0].id - 1;
+    this.allCards[index1].match();
+    this.allCards[index2].match();
+    this.matches++;
+    var cards = {one: card1, two: card2};
+    this.matchedCards.push(cards);
+  }
+
+}
