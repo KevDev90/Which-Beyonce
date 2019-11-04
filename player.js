0c7aaa68fa0d426e092f912b4392fd339c10cafb
kevin johnson <kevinj902302@gmail.com>
2019-11-04 13:32:22 -0700

    Push changes on new branch

--- player.js
+++ player.js
@@ -2,7 +2,7 @@ class Player {
   constructor(player) {
     this.matchCount = 0;
     this.name = player.name
-    this.beginTime = player.beginTime;
     this.entireTime = null;
+    this.beginTime = player.beginTime;
   }
 };
